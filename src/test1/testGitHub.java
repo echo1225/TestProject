8bdcc87acde053f360c66cdce5c4eb90db1e445f
echo1225 <qkumba@hotmail.com>
2017-04-12 01:58:59 -0400

    second print line

--- src/test1/testGitHub.java
+++ src/test1/testGitHub.java
@@ -4,7 +4,7 @@ public class testGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("This is my first Git Hub attempt");
-
+		System.out.println("This is my second line");
 	}
 
 }
